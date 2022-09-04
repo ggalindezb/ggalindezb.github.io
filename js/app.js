07ee77e3404dab1d0a42a3688a77fb6b436fe3a9
Gerardo Galindez <ggalindezb@outlook.com>
2022-09-04 18:10:52 -0500

    Complete typing animation

--- js/app.js
+++ js/app.js
@@ -1,3 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('Loaded.');
+  setTimeout(type, 2000, 'Hello, I am Gerardo')
 }, false);
+
+const clamp = (value, min, max) => {
+  return Math.min(max, Math.max(value, min))
+}
+
+const type = (target) => {
+  console.log(`Typing: ${target}`)
+  punch('', target.split(''))
+}
+
+const punch = (string, letters) => {
+  let letter = letters.shift();
+  string = `${string}${letter}`;
+  document.getElementById('prompt').textContent = string
+
+  if(letters.length > 0) {
+    let ttl = clamp(Math.random() * 350, 200, 350)
+    setTimeout(punch, ttl, string, letters);
+  }
+}
